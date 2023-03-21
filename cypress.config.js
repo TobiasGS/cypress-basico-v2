a89327bdea132b5f9e0d425137b60df5bd993cf3
Tobias Gomes <tobiassantos_96@hotmail.com>
2023-03-20 21:02:33 -0300

    1

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   viewportHeight: 880,
-  viewportWidth: 1280,
-  e2e: {
-    setupNodeEvents(on, config) {},
+  viewportWidth: 1280
 
-  },
-  
 })
 
